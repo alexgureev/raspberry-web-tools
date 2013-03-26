ccd04ecd4d70ea34dbb1a66f82d5b1a107ca17a5
barif <w3db@yandex.ru>
2013-03-26 15:23:52 +0200

    automated update

--- tools/git/git.sh
+++ tools/git/git.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-# Auto commit to git
-#WEB_DIR=/var/www
-DATE=$(date +"%m_%d_%Y")
-HOME=~/
-echo $HOME
+
+PATH="~/bin:/sbin:/bin:/var/sbin:/var/bin:/usr/local/bin:$PATH"
+
+cd "$1"
+git add -A .
+git commit -q -m 'automated update'
+git push -q
+#DATE=$(date +"%m_%d_%Y")
+#HOME=~/
+#echo $HOME
 #cd $WEB_DIR
-git config --global user.name "barif"
-git config --global user.email "w3db@yandex.ru"
-git commit -a -m "update $DATE"
-git push origin
+#git config --global user.name "barif"
+#git config --global user.email "w3db@yandex.ru"
+#git commit -a -m "update $DATE"
+#git push origin
