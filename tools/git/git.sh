c7aa85ca3ab5d44e4b06b2e89ac957a6eaff660a
barif <w3db@yandex.ru>
2013-03-26 14:26:22 +0200

    update 03_26_2013

--- tools/git/git.sh
+++ tools/git/git.sh
@@ -4,7 +4,7 @@
 #WEB_DIR=/var/www
 DATE=$(date +"%m_%d_%Y")
 #cd $WEB_DIR
-git config --global user.name "barif"
-git config --global user.email "w3db@yandex.ru"
-git commit -a -m "update $DATE"
-git push origin
+sudo git commit -a -m "update $DATE"
+sudo git config --global user.name "barif"
+sudo git config --global user.email "w3db@yandex.ru"
+sudo git push origin
