45f69d33bbae61e963ed527a9c8046ec02eaf811
www-data <www-data@raspberrypi.(none)>
2013-03-26 15:09:38 +0200

    update 03_26_2013

--- tools/git/git.sh
+++ tools/git/git.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Auto commit to git
-su
 #WEB_DIR=/var/www
 DATE=$(date +"%m_%d_%Y")
-$ export HOME=
-$ unset HOME
-% setenv HOME
+$ echo '$HOME'
+$HOME
 #cd $WEB_DIR
 git config --global user.name "barif"
 git config --global user.email "w3db@yandex.ru"
