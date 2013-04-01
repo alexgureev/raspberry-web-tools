4a512b4b2db72be11d4dc0d898b628981f2c0ebb
barif <w3db@yandex.ru>
2013-04-02 00:01:01 +0300

    automated update2

--- tools/android/widget.php
+++ tools/android/widget.php
@@ -1,19 +1,14 @@
 <?php
 $data = array(
-    "title" => "Universal widget",
+    "title" => "Aqua",
     "type"  => "list",
-    "date"  => date("d.m.y H:i:s"),
+    "date"  => date("H:i:s"),
     "data"  => array(
-            1 => array("name"=>"Row one", "value"=>"String"),
-            2 => array("name"=>"Row two", "value"=>"1200"),
-            3 => array("name"=>"Row three", "value"=>"50%"),
-            4 => array("name"=>"Row 4", "value"=>"1`230,00$"),
-            5 => array("name"=>"Row 5", "value"=>"19403"),
-            6 => array("name"=>"Row 6", "value"=>"l322"),
-            7 => array("name"=>"Row 7", "value"=>"l322"),
-            8 => array("name"=>"Row 8", "value"=>"l322"),
-            9 => array("name"=>"Row 9", "value"=>"l322"),
-            10 => array("name"=>"Row 10", "value"=>"l322"),
+            1 => array("name"=>"Temp", "value"=>"25"),
+            2 => array("name"=>"Light", "value"=>"On"),
+            3 => array("name"=>"Filter", "value"=>"On"),
+            4 => array("name"=>"CO2", "value"=>"On"),
+            5 => array("name"=>"O2", "value"=>"On")
     )
 );
 echo json_encode($data);
