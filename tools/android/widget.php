0e703ed5a71b7164be74a472014e8c76ee4831bf
barif <w3db@yandex.ru>
2013-04-21 00:01:01 +0300

    automated update2

--- tools/android/widget.php
+++ tools/android/widget.php
@@ -4,11 +4,11 @@ $data = array(
     "type"  => "list",
     "date"  => date("H:i:s"),
     "data"  => array(
-            1 => array("name"=>"Temp", "value"=>"25"),
-            2 => array("name"=>"Light", "value"=>"On"),
-            3 => array("name"=>"Filter", "value"=>"On"),
-            4 => array("name"=>"CO2", "value"=>"On"),
-            5 => array("name"=>"O2", "value"=>"On")
+            1 => array("name"=>"Temp", "value"=>"25", "color"=>"red"),
+            2 => array("name"=>"Light", "value"=>"On", "color"=>"blue"),
+            3 => array("name"=>"Filter", "value"=>"On", "color"=>"red"),
+            4 => array("name"=>"CO2", "value"=>"On", "color"=>"green"),
+            5 => array("name"=>"O2", "value"=>"On", "color"=>"green")
     )
 );
 echo json_encode($data);
